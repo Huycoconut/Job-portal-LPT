a28fb915eb1b3b12b461a694e062d5e8afd71612
Huycoconut <107424687+Huycoconut@users.noreply.github.com>
2025-03-20 22:36:16 +0700

    Hash user password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,33 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
+import { bcrypt, hashSync } from 'bcrypt';
+import { genSaltSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  //Thông báo là UsersService sẽ sử dụng model User đã được khai báo từ user.module
+  constructor(
+    @InjectModel(User.name)
+    // Model<User> là kiểu cho usermodel
+   private userModel: Model<User>,
+  ) {}
+
+  hashPassword = (password: string) => {
+    const saltRounds = genSaltSync(10);
+    const hash = hashSync(password, saltRounds);
+    return hash;
+  };
 
   async create(email: string, password: string, name: string) {
+    const hashPassword = this.hashPassword(password);
     const user = await this.userModel.create({
       email,
-      password,
+      password: hashPassword,
       name,
     });
     return user;
