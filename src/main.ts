cf320195c8127faee9cf52f3b81d5244e5ea38cc
Huycoconut <107424687+Huycoconut@users.noreply.github.com>
2025-05-22 22:03:21 +0700

    add version api for backend

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transfomr.interceptor';
 
@@ -21,6 +21,13 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   app.enableCors();
+
+  //config version
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'], //v1, v2
+  });
 
   await app.listen(configService.get<string>('PORT'));
 }
